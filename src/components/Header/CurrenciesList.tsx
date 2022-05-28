7842464be3a8814623c0649353dbd4c7ee5a92e8
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-05-27 21:00:17 -0300

    useEffect in currency list

--- src/components/Header/CurrenciesList.tsx
+++ src/components/Header/CurrenciesList.tsx
@@ -1,26 +1,36 @@
+import { AxiosResponse } from 'axios';
 import { CaretDown, CaretUp } from 'phosphor-react'
-import { useState } from "react";
+import { useState, useEffect, Key } from "react";
 import { api } from "../../services/api";
+
+interface ICurrency {
+  id: string;
+  currencyName: string;
+}
+
+interface ICurrenciesList extends Array<ICurrency>{}
 
 export function CurrenciesList(){
   const [isClicked, setIsClicked] = useState(false)
-  const [currenciesList, setCurrenciesList] = useState({})
+  const [currenciesList, setCurrenciesList] = useState<ICurrenciesList>([])
 
-  async function getCurrenciesList() {
-    if(isClicked === true) {
-      setIsClicked(false)
-    } else { 
-      const response = await api.get('currencies')
+  useEffect(() => {
+    async function getCurrencies() {
+      const response: AxiosResponse = await api.get('currencies')
 
-    setCurrenciesList(response.data.results)
-    setIsClicked(true)
+      setCurrenciesList(Object.values(response.data.results))
     }
+    getCurrencies();
+  }, []) 
+
+  function showCurrencies(){
+    isClicked === true ? setIsClicked(false) : setIsClicked(true)
   }
 
   return(
     <>
       <button 
-      onClick={getCurrenciesList}
+      onClick={showCurrencies}
       className=" flex items-center text-xl text-white">
       Currencies
       {isClicked ? <CaretUp /> : <CaretDown /> }
@@ -29,11 +39,11 @@ export function CurrenciesList(){
       {!isClicked ? null : 
         <div 
           className="absolute min-w-[160px] overflow-auto z-10 right-0 top-16 max-h-[818px]">
-          {Object.entries(currenciesList).map(([key, value]) => {
+          {currenciesList.map(index => {
             return(
-              <div key={key} className="bg-[#205375] hover:bg-opacity-70 flex items-center max-w-[250px]">
-                <span className="text-white py-2 px-2 inline-block max-w-[40px]">{key}</span>
-                <span className="text-white py-2 px-4 inline-block text-sm text-left">{value.currencyName}</span>
+              <div key={index.id} className="bg-[#205375] hover:bg-opacity-70 flex items-center max-w-[250px]">
+                <span className="text-white py-2 px-2 inline-block max-w-[40px]">{index.id}</span>
+                <span className="text-white py-2 px-4 inline-block text-sm text-left">{index.currencyName}</span>
               </div>
             )
           })}
