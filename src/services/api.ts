4aa15b5ec81027d90152a7992c7e74b78fdf93dd
Douglas <douglasclaudino10.dc@gmail.com>
2023-09-14 17:10:30 -0300

    update axios baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_URL
+  baseURL: `http://localhost:${import.meta.env.VITE_URL}`,
+  timeout: 5000,
 })
